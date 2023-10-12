649ad6a646e1b908461bde254939d18f41a9117a
marshall.m <marshall.m@nethouse.net>
2023-10-12 16:04:51 +0300

    add to basket init commit

---
+++ includes/class-add_to_basket-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://marshallfungai.github.io
+ * @since      1.0.0
+ *
+ * @package    Add_to_basket
+ * @subpackage Add_to_basket/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Add_to_basket
+ * @subpackage Add_to_basket/includes
+ * @author     Fungai Marshall <marshall@devartists.com>
+ */
+class Add_to_basket_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-add_to_basket-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       https://marshallfungai.github.io
+ * @since      1.0.0
+ *
+ * @package    Add_to_basket
+ * @subpackage Add_to_basket/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Add_to_basket
+ * @subpackage Add_to_basket/includes
+ * @author     Fungai Marshall <marshall@devartists.com>
+ */
+class Add_to_basket_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'add_to_basket',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
